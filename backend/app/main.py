ad317e21a46e5f4be57b1e70b3ac7f7349e1d78b
Kim Sihyun <kimsihyun35@gmail.com>
2026-05-11 16:32:17 +0900

    Revert "Add log save and log retrieval feature"
    This reverts commit 87875c455d03dae762e9472eabb8b7f52857b655.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,7 @@
 from fastapi import FastAPI
-from services.saved_route_service import save_log, get_logs
 
 app = FastAPI()
 
 @app.get("/")
 def read_root() :
     return {"Hello" : "OpenSource"}
-
-@app.post("/logs")
-def create_log(data: dict):
-
-	user_input = data.get("user_input")
-	result = data.get("result")
-
-	saved_log = save_log(user_input, result)
-
-	return {
-		"message": "로그 저장 완료",
-		"log": saved_log
-	}
-
-@app.get("/log")
-def read_logs()"
-
-	return {
-		"logs": getlogs()
-	}
-
